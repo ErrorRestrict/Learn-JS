8df8ec714c923f6c434ffe11235f504f1a9919d9
ErrorRestrict <pvrtripathi@gmail.com>
2026-03-27 18:38:30 +0530

    yes

---
+++ Challange1/ch.js
@@ -0,0 +1,8 @@
+const heading = document.createElement('h1');
+heading.innerText ='MY FIRST CHALLNEGGE';
+
+document.body.appendChild(heading);
+
+const para = document.createElement('p');
+para.innerText = " This is my first challenge and I am confident it to done very well";
+document.body.appendChild(para);
